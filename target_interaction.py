c071068a801295cc085833668faab64cda688b35
sarkardt7657-afk <sarkardt7657@gmail.com>
2026-04-02 19:23:44 +0530

    tareget interaction

---
+++ target_interaction.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df = pd.read_csv("dataset.csv")
+
+def check_interaction(drug, target):
+
+    result = df[(df["Drug"].str.lower() == drug.lower()) & (df["Target"].str.lower() == target.lower())]
+    if not result.empty:
+        interaction = result.iloc[0]
+        return f"Found, Type: {interaction["Type"]}"
+    else:
+        "Not Found"
+
+drug = str(input("Enter Drug: "))
+target = str(input("Enter Target: "))
+
+print(check_interaction(drug,target))
